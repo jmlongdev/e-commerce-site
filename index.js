aaaeae15f0abd7552f873aa683546e2c3b8600f3
Max <jmlong.dev@gmail.com>
2022-06-13 18:42:51 -0400

    added styling for products page for user

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser"); // parses information and puts it on the req.body property
 const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
-const productsRouter = require("./routes/admin/products");
-
+const adminProductsRouter = require("./routes/admin/products");
+const productsRouter = require("./routes/products");
 const app = express();
 
 // Globally lets all route handlers apply this middleware function
@@ -18,6 +18,7 @@ app.use(
 );
 //add after the middlewares
 app.use(authRouter);
+app.use(adminProductsRouter);
 app.use(productsRouter);
 
 app.listen(3000, () => {
